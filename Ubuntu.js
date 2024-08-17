afd76a19dc936327f6dcfa8ce988a7b6e0fa2f69
Nicolai Kuertoes <69422846+NicolaiKuertoes@users.noreply.github.com>
2024-08-17 17:31:02 +0200

    add Ubuntu

---
+++ Ubuntu.js
@@ -0,0 +1,23 @@
+black           = '#2E3436';
+red             = '#CC0000';
+green           = '#4E9A06';
+yellow          = '#C4A000';
+blue            = '#3465A4';
+magenta         = '#75507B';
+cyan            = '#06989A';
+white           = '#D3D7CF';
+lightBlack      = '#555753';
+lightRed        = '#EF2929';
+lightGreen      = '#8AE234';
+lightYellow     = '#FCE94F';
+lightBlue       = '#729FCF';
+lightMagenta    = '#AD7FA8';
+lightCyan       = '#34E2E2';
+lightWhite      = '#EEEEEC';
+
+
+t.prefs_.set('color-palette-overrides',
+    [black, red, green, yellow, blue, magenta, cyan, white,
+     lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite]);
+t.prefs_.set('background-color', '#300A24');
+t.prefs_.set('cursor-color', '#ffffff');
